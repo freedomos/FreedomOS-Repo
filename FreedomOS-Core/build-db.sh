1c646c3581302ac77c1426b21e7bdb9b699c144e
frazzlesgrant1994 <frazer_grant@outlook.com>
2022-08-01 10:26:51 +0100

    build core

--- FreedomOS-Core/build-db.sh
+++ FreedomOS-Core/build-db.sh
@@ -10,13 +10,13 @@
 # get hidden bugs that are hard to discover.
 set -euo pipefail
 
-x86_pkgbuild=$(find ../FreedomOS-pkgbuild/x86_64 -type f -name "*.pkg.tar.zst*")
+#x86_pkgbuild=$(find ../FreedomOS-pkgbuild/x86_64 -type f -name "*.pkg.tar.zst*")
 
-for x in ${x86_pkgbuild}
-do
-    mv "${x}" x86_64/
-    echo "Moving ${x}"
-done
+#for x in ${x86_pkgbuild}
+#do
+#    mv "${x}" x86_64/
+#    echo "Moving ${x}"
+#done
 
 echo "###########################"
 echo "Building the repo database."
@@ -24,7 +24,7 @@ echo "###########################"
 
 ## Arch: x86_64
 cd x86_64
-rm -f FreedomOS-Core-Repo*
+rm -f FreedomOS-Core*
 
 echo "###################################"
 echo "Building for architecture 'x86_64'."
@@ -34,18 +34,18 @@ echo "###################################"
 ## -s: signs the packages
 ## -n: only add new packages not already in database
 ## -R: remove old package files when updating their entry
-repo-add -n -R FreedomOS-Core-Repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R FreedomOS-Core.db.tar.gz *.pkg.tar.zst
 
 # Removing the symlinks because GitLab can't handle them.
-rm FreedomOS-Core-Repo.db
+rm FreedomOS-Core.db
 #rm FreedomOS-Core-Repo.db.sig
-rm FreedomOS-Core-Repo.files
+rm FreedomOS-Core.files
 #rm FreedomOS-Core-Repo.files.sig
 
 # Renaming the tar.gz files without the extension.
-mv FreedomOS-Core-Repo.db.tar.gz FreedomOS-Core-Repo.db
+mv FreedomOS-Core.db.tar.gz FreedomOS-Core.db
 #mv FreedomOS-Core-Repo.db.tar.gz.sig FreedomOS-Core-Repo-db.sig
-mv FreedomOS-Core-Repo.files.tar.gz FreedomOS-Core-Repo.files
+mv FreedomOS-Core.files.tar.gz FreedomOS-Core.files
 #mv FreedomOS-Core-Repo.files.tar.gz.sig FreedomOS-Core-Repo.files.sig
 
 echo "#######################################"
