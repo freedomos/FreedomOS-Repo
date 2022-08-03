fc9a5abb6d411a21e256ba52b78071e73adf4fb0
freedomos <frazergrant345@gmail.com>
2022-08-03 23:56:40 +0100

    test

--- build_push.sh
+++ build_push.sh
@@ -49,4 +49,27 @@ echo "#######################################"
 echo "Packages in the repo have been updated!"
 echo "#######################################"
  
+git pull
 
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin master
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
