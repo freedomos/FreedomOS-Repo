eaba2afbd062a173b9fbcc1b3bc488546e82c5bf
frazzlesgrant1994 <frazer_grant@outlook.com>
2022-08-11 20:38:41 +0100

    Repo Update

--- build_push.sh
+++ build_push.sh
@@ -21,29 +21,46 @@ echo "###################################"
 ## -s: signs the packages
 ## -n: only add new packages not already in database
 ## -R: remove old package files when updating their entry
-repo-add -n -R FreedomOS-Core/x86_64/FreedomOS-Core.db.tar.gz FreedomOS-Core/x86_64/*.pkg.tar.zst
-repo-add -n -R FreedomOS-Extra/x86_64/FreedomOS-Extra.db.tar.gz FreedomOS-Extra/x86_64/*.pkg.tar.zst
-repo-add -n -R FreedomOS-Testing/x86_64/FreedomOS-Testing.db.tar.gz FreedomOS-Testing/x86_64/*.pkg.tar.zst
+repo-add -n -s -v -R FreedomOS-Core/x86_64/FreedomOS-Core.db.tar.gz FreedomOS-Core/x86_64/*.pkg.tar.zst
+repo-add -n -s -v -R FreedomOS-Extra/x86_64/FreedomOS-Extra.db.tar.gz FreedomOS-Extra/x86_64/*.pkg.tar.zst
+repo-add -n -s -v -R FreedomOS-Testing/x86_64/FreedomOS-Testing.db.tar.gz FreedomOS-Testing/x86_64/*.pkg.tar.zst
 
 # Removing the symlinks because GitLab can't handle them.
 rm -f FreedomOS-Core/x86_64/FreedomOS-Core.db
 rm -f FreedomOS-Extra/x86_64/FreedomOS-Extra.db
 rm -f FreedomOS-Testing/x86_64/FreedomOS-Testing.db
-#rm FreedomOS-Core-Repo.db.sig
+rm -f FreedomOS-Core/x86_64/FreedomOS-Core.db.sig
+rm -f FreedomOS-Extra/x86_64/FreedomOS-Extra.db.sib
+rm -f FreedomOS-Testing/x86_64/FreedomOS-Testing.db.sig
+
 rm -f FreedomOS-Core/x86_64/FreedomOS-Core.files
 rm -f FreedomOS-Extra/x86_64/FreedomOS-Extra.files
 rm -f FreedomOS-Testing/x86_64/FreedomOS-Testing.files
-#rm FreedomOS-Core-Repo.files.sig
+rm -f FreedomOS-Core/x86_64/FreedomOS-Core.files.sig
+rm -f FreedomOS-Extra/x86_64/FreedomOS-Extra.files.sig
+rm -f FreedomOS-Testing/x86_64/FreedomOS-Testing.files.sig
+
+
 
 # Renaming the tar.gz files without the extension.
 mv FreedomOS-Core/x86_64/FreedomOS-Core.db.tar.gz FreedomOS-Core/x86_64/FreedomOS-Core.db
 mv FreedomOS-Extra/x86_64/FreedomOS-Extra.db.tar.gz FreedomOS-Extra/x86_64/FreedomOS-Extra.db
 mv FreedomOS-Testing/x86_64/FreedomOS-Testing.db.tar.gz FreedomOS-Testing/x86_64/FreedomOS-Testing.db
-#mv FreedomOS-Core-Repo.db.tar.gz.sig FreedomOS-Core-Repo-db.sig
+
+mv FreedomOS-Core/x86_64/FreedomOS-Core.db.tar.gz.sig FreedomOS-Core/x86_64/FreedomOS-Core.db.sig
+mv FreedomOS-Extra/x86_64/FreedomOS-Extra.db.tar.gz.sig FreedomOS-Extra/x86_64/FreedomOS-Extra.db.sig
+mv FreedomOS-Testing/x86_64/FreedomOS-Testing.db.tar.gz.sig FreedomOS-Testing/x86_64/FreedomOS-Testing.db.sig
+
+
+
 mv FreedomOS-Core/x86_64/FreedomOS-Core.files.tar.gz FreedomOS-Core/x86_64/FreedomOS-Core.files
 mv FreedomOS-Extra/x86_64/FreedomOS-Extra.files.tar.gz FreedomOS-Extra/x86_64/FreedomOS-Extra.files
 mv FreedomOS-Testing/x86_64/FreedomOS-Testing.files.tar.gz FreedomOS-Testing/x86_64/FreedomOS-Testing.files
-#mv FreedomOS-Core-Repo.files.tar.gz.sig FreedomOS-Core-Repo.files.sig
+
+mv FreedomOS-Core/x86_64/FreedomOS-Core.files.tar.gz.sig FreedomOS-Core/x86_64/FreedomOS-Core.files.sig
+mv FreedomOS-Extra/x86_64/FreedomOS-Extra.files.tar.gz.sig FreedomOS-Extra/x86_64/FreedomOS-Extra.files.sig
+mv FreedomOS-Testing/x86_64/FreedomOS-Testing.files.tar.gz.sig FreedomOS-Testing/x86_64/FreedomOS-Testing.files.sig
+
 
 echo "#######################################"
 echo "Packages in the repo have been updated!"
